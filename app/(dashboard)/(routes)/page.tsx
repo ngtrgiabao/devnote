611c9cbcc39434081381727f8ead4f360311a6a9
ngtrgiabao <ngtrgiabao251002@gmail.com>
2024-03-11 16:07:10 +0700

    [feat]: responsive mobile

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,63 +1,61 @@
 const Home = () => {
     return (
-        <div className="flex items-center justify-center mt-[10%] text-sm md:text-base px-4 md:px-0 break-words">
+        <div className="flex items-center justify-center mt-[10%] text-sm md:text-base px-2 md:px-0 break-words">
             <div className="text-center text-white">
                 <samp>
                     👋 Hello there, I&apos;m Bao (Yanji), Frontend developer
                     from CanTho, Vietnam.
                 </samp>
-                <p>
-                    <samp>
-                        <span>learning</span> .
-                        <a
-                            className="text-amber-400 underline me-2"
-                            href="https://yanji-porfolio.vercel.app/"
-                        >
-                            me
-                        </a>
-                        .
-                        <a className="text-amber-400 underline me-2" href="/">
-                            blog
-                        </a>
-                        .
-                        <a className="text-amber-400 underline me-2" href="/">
-                            resume
-                        </a>
-                        .
-                        <a className="text-amber-400 underline me-2" href="/">
-                            projects
-                        </a>
-                        .
-                        <a className="text-amber-400 underline me-2" href="/">
-                            tweets
-                        </a>
-                        .
-                        <a className="text-amber-400 underline me-2" href="/">
-                            contacts
-                        </a>
-                        <br />
-                        <a
-                            className="text-amber-400 underline me-2"
-                            href="mailto:ngtrgiabao251002@gmail.com"
-                        >
-                            mail
-                        </a>
-                        .
-                        <a
-                            className="text-amber-400 underline me-2"
-                            href="https://gist.github.com/ngtrgiabao"
-                        >
-                            gist
-                        </a>
-                        .
-                        <a
-                            className="text-amber-400 underline me-2"
-                            href="https://github.com/ngtrgiabao/use"
-                        >
-                            use
-                        </a>
-                    </samp>
-                </p>
+                <div>
+                    <div className="mb-2">learning</div> .
+                    <a
+                        className="text-amber-400 underline me-2"
+                        href="https://yanji-porfolio.vercel.app/"
+                    >
+                        me
+                    </a>
+                    .
+                    <a className="text-amber-400 underline me-2" href="/">
+                        blog
+                    </a>
+                    .
+                    <a className="text-amber-400 underline me-2" href="/">
+                        resume
+                    </a>
+                    .
+                    <a className="text-amber-400 underline me-2" href="/">
+                        projects
+                    </a>
+                    .
+                    <a className="text-amber-400 underline me-2" href="/">
+                        tweets
+                    </a>
+                    .
+                    <a className="text-amber-400 underline me-2" href="/">
+                        contacts
+                    </a>
+                    <br />
+                    <a
+                        className="text-amber-400 underline me-2"
+                        href="mailto:ngtrgiabao251002@gmail.com"
+                    >
+                        mail
+                    </a>
+                    .
+                    <a
+                        className="text-amber-400 underline me-2"
+                        href="https://gist.github.com/ngtrgiabao"
+                    >
+                        gist
+                    </a>
+                    .
+                    <a
+                        className="text-amber-400 underline me-2"
+                        href="https://github.com/ngtrgiabao/use"
+                    >
+                        use
+                    </a>
+                </div>
             </div>
         </div>
     );
